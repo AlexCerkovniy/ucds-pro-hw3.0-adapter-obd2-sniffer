eb0f42e8caa77b268f6281296cf25d48ab47ee93
Alex Tserkovniy <tserkovniy.o@ajax.systems>
2025-02-23 03:30:25 +0200

    Update

--- Core/Src/can.c
+++ Core/Src/can.c
@@ -44,8 +44,8 @@ void MX_CAN2_Init(void)
   hcan2.Init.Prescaler = 9;
   hcan2.Init.Mode = CAN_MODE_NORMAL;
   hcan2.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan2.Init.TimeSeg1 = CAN_BS1_3TQ;
-  hcan2.Init.TimeSeg2 = CAN_BS2_4TQ;
+  hcan2.Init.TimeSeg1 = CAN_BS1_6TQ;
+  hcan2.Init.TimeSeg2 = CAN_BS2_1TQ;
   hcan2.Init.TimeTriggeredMode = DISABLE;
   hcan2.Init.AutoBusOff = DISABLE;
   hcan2.Init.AutoWakeUp = ENABLE;
